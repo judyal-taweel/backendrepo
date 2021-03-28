708ba0fb570c6cc5020cf68c3ab36b1cf5db122d
judyal-taweel <judybataineh@gmail.com>
2021-03-28 20:32:53 +0300

    lab06

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const PORT = 3000;
+
+const express =require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/location',handlelocation);
+app.get('/weather',handleweather);
+
+function handlelocation(request,response){
+    const getLocation = require('./data/location.json');
+    const city = request.query.city;
+    let obj = {
+        name: getLocation[0].display_name,
+        search_query: city,
+        formatted_query : city,
+        latitude : getLocation[0].lat,
+        longitude : getLocation[0].lon
+    };
+    response.send(obj);
+}
+
+
+function handleweather(request,response){
+    const data = require('./data/weather.json');
+    const weathers = data.weather;
+    const weatherResponse = [];
+    weathers.forEach(item => {
+        const current = item.weather;
+        weatherResponse.push({
+            weather = current.description,    
+        })
+        datetime = current.datetime;
+        
+    });
+    }
+
+    app.listen(PORT, ()=> console.log(`App is running on Server on port: ${PORT}`));
