cc44c5c3334fac88471b3b92fc5c9052084b60b4
judyal-taweel <judybataineh@gmail.com>
2021-03-29 23:37:45 +0300

    lab07

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 require("dotenv").config();
+const superagent = require('superagent');
+
 const PORT = process.env.PORT;
 
 const express =require('express');
@@ -12,15 +14,37 @@ app.use(cors());
 app.get('/location',handlelocation);
 app.get('/weather',handleweather);
 
+const myLocalLocations = {};
+
 function handlelocation(request,response){
     const city = request.query.city;
-    const getLocation = require('./data/location.json');
-    const newlocation = new Location(getLocation[0])
-    response.send(newlocation);
+
+    if (myLocalLocations[city]) {
+        response.send(myLocalLocations[city]);
+      
+      } else {    let key = process.env.GEOCODE_API_KEY;
+        const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+        superagent.get(url).then(res=> {
+          const locationData = res.body[0];
+          const location = new Location(city, locationData);
+          myLocalLocations[city] = location;
+          response.send(location);
+    
+        }).catch((err)=> {
+          console.log("ERROR IN LOCATION API");
+          console.log(err)
+        });
+      
+    
+    }
+    // const getLocation = require('./data/location.json');
+    // const newlocation = new Location(city , getLocation[0])
+    // response.send(newlocation);
 }
 
 
 function Location(data) {
+    this.search_query = city;
     this.formatted_query = data.display_name;
     this.latitude = data.lat;
     this.longitude = data.lon;
@@ -49,8 +73,21 @@ function handleweather(request,response){
 
     app.listen(PORT, ()=> console.log(`App is running on Server on port: ${PORT}`));
 
-    app.use('*', (request, response) => {
-        let status =404;
-        response.status(status).send({status:status , msg:'Not found'});
-      });
+    // app.use('*', (request, response) => {
+    //     let status =404;
+    //     response.status(status).send({status:status , msg:'Not found'});
+    //   });
+
+    app.use('*', notFoundHandler); // 404 not found url
+ 
+app.use(errorHandler);
+
+function notFoundHandler(request, response) {
+  response.status(404).send('requested API is Not Found!');
+}
+
+function errorHandler(err, request, response, next) {
+  response.status(500).send('something is wrong in server');
+}
+
       
